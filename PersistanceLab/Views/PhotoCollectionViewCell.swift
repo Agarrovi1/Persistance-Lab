5789299d2f89704aa74019936b9a432e033d0bb0
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-30 15:24:52 -0400

    makes photoCollectionViewCell

---
+++ PersistanceLab/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PersistanceLab
+//
+//  Created by Angela Garrovillas on 9/30/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
