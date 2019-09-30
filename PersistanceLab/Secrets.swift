217bae389dced97a2d1b60052dc9b57d6ab5e7fe
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-09-30 14:56:37 -0400

    made mvc folders

--- PersistanceLab/Secrets.swift
+++ PersistanceLab/Secrets.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+struct Secrets {
+    static let apiKey = "13796459-1f28e6be7544e57cf485862e5"
+    
+    static func getUrlWith(query: String) -> String {
+        let query = query.lowercased()
+        return "https://pixabay.com/api/?key=\(self.apiKey)&q=\(query)"
+    }
+}
